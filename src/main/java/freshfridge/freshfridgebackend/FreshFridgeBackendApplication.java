c5ce3b3696efb7652e508c71203aeda754902f02
walathamasahid <s1154748@student.hsleiden.nl>
2025-11-28 16:58:50 +0100

    Initial Spring Boot project set-up

---
+++ src/main/java/freshfridge/freshfridgebackend/FreshFridgeBackendApplication.java
@@ -0,0 +1,13 @@
+package freshfridge.freshfridgebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshFridgeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreshFridgeBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/freshfridge/freshfridgebackend/FreshFridgeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package freshfridge.freshfridgebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshFridgeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
