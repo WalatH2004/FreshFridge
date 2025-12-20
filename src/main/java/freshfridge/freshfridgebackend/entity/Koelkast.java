ce7efe758b735cd96550f970a9dcc27b493f8779
walathamasahid <s1154748@student.hsleiden.nl>
2025-12-20 15:39:41 +0100

    Koelkast entity toegevoegd

---
+++ src/main/java/freshfridge/freshfridgebackend/entity/Koelkast.java
@@ -0,0 +1,57 @@
+package freshfridge.freshfridgebackend.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Koelkast")
+public class Koelkast {
+
+    @Id
+    @Column(name = "koelkastId")
+    private Integer koelkastId;
+
+    @Column(name = "naam", nullable = false)
+    private String naam;
+
+    @Column(name = "locatie")
+    private String locatie;
+
+    @ManyToOne
+    @JoinColumn(name = "gebruikernr", nullable = false)
+    private Gebruiker gebruiker;
+
+    public Koelkast() {
+    }
+
+    public Integer getKoelkastId() {
+        return koelkastId;
+    }
+
+    public void setKoelkastId(Integer koelkastId) {
+        this.koelkastId = koelkastId;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public void setNaam(String naam) {
+        this.naam = naam;
+    }
+
+    public String getLocatie() {
+        return locatie;
+    }
+
+    public void setLocatie(String locatie) {
+        this.locatie = locatie;
+    }
+
+    public Gebruiker getGebruiker() {
+        return gebruiker;
+    }
+
+    public void setGebruiker(Gebruiker gebruiker) {
+        this.gebruiker = gebruiker;
+    }
+}
